05c6d54838150ef1ffa2de8c71367cd7304fd786
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2021-03-13 22:02:10 -0500

    added kmip

---
+++ security/ear/kmip/runMongo.sh
@@ -0,0 +1,5 @@
+mongod --port 27017 --dbpath /var/lib/mongo \
+  --enableEncryption --kmipServerName localhost \
+  --kmipPort 5696 --kmipServerCAFile /etc/pykmip/certs/server.pem \
+  --kmipClientCertificateFile /etc/pykmip/certs/server.pem
+
