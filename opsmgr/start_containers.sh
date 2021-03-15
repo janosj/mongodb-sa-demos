f81ed03f9a14e80d9d05b4ab675a6314dda7c021
janosj <janos@yahoo.com>
2021-03-14 21:40:50 -0400

    Added scripts from OpsMgr VM

---
+++ opsmgr/start_containers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Run this script using 'sudo' to create some automation agent containers, e.g.:  > sudo ./start_containers.sh
+
+NUMBER_TO_CREATE=3
+PROJECTID=5dc701d59b4d0026128370af
+APIKEY=5f5fa96dfb107b09cd2d32608f8ca667eb2bf4f411d6f42080279b72
+
+echo 
+
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: Please run script with sudo"
+  echo 
+  exit 1
+fi
+
+ADD_HOST_LIST_PARAMS="--add-host opsmgr:172.17.0.1"
+
+# Build list of hostname/ip-address mappings representing ops mgr host + all containers, ready to then be passed to each container created - also adds hostname/ip-address mappings to /etc/hosts of VM
+for i in $(seq 1 $NUMBER_TO_CREATE)
+do
+    octet=$((i+1))
+    ADD_HOST_LIST_PARAMS="${ADD_HOST_LIST_PARAMS} --add-host myserver${i}:172.17.0.${octet}"
+    echo "172.17.0.${octet}  myserver${i}" >> /etc/hosts
+done
+
+# Loop creating a set of Docker containers
+
+# Parameters added by J Janos
+# 2702x allows access to MDB (27021|2|3)
+# 2703x allows .... ?
+# 3306x allows .... ?
+# -v provides access to content on the host from the containers.
+JJ_ADDED_PARAMS="-p 2702\${i}:2702\${i} -p 2703\${i}:2703\${i} -p 3306\${i}:3306\${i} -v /etc/pykmip/certs:/etc/pykmip/certs"
+
+for i in $(seq 1 $NUMBER_TO_CREATE)
+do
+    cmd=" docker run "${JJ_ADDED_PARAMS}" "${ADD_HOST_LIST_PARAMS}" --hostname=myserver${i} -d --name automation-agent-container${i} -t mongo/automation-agent /opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent --mmsGroupId=${PROJECTID} --mmsApiKey=${APIKEY} --mmsBaseUrl=http://opsmgr:8080 -logLevel=INFO -logFile=/var/log/mongodb-mms-automation/automation-agent.log"
+echo $cmd
+    #cmd=" docker run -p 2702${i}:2702${i} -p 2703${i}:2703${i} -p 3306${i}:3306${i} "${ADD_HOST_LIST_PARAMS}" --hostname=myserver${i} -d --name automation-agent-container${i} -t mongo/automation-agent /opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent --mmsGroupId=${PROJECTID} --mmsApiKey=${APIKEY} --mmsBaseUrl=http://opsmgr:8080 -logLevel=INFO -logFile=/var/log/mongodb-mms-automation/automation-agent.log"
+    eval "${cmd}"
+done
+
+echo 
+echo "${NUMBER_TO_CREATE} containers running Ops Mgr Automation Agents should have started - check output below (/etc/hosts has also been added to)"
+echo
+docker ps -a
+echo 
+
