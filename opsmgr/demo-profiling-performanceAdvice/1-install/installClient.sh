44cb1c03793b6a61eba9d105382281a08780f7cb
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2021-05-25 14:49:41 -0400

    fix

--- opsmgr/demo-profiling-performanceAdvice/1-install/installClient.sh
+++ opsmgr/demo-profiling-performanceAdvice/1-install/installClient.sh
@@ -1,21 +1,13 @@
 # Check for sudo user.
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
-#wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-yum install -y wget
-
-# Download manually
-# wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-yum install -y ./epel-release-latest-*.noarch.rpm
-
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum install -y nodejs
 npm install -g mgeneratejs
-
-yum install -y ./mongodb-enterprise-shell-4.2.5-1.el7.x86_64.rpm
-yum install -y ./mongodb-enterprise-tools-4.2.5-1.el7.x86_64.rpm
+# Note: this is the new mongosh that replaces the original mongo shell.
+yum install -y https://downloads.mongodb.com/compass/mongodb-mongosh-0.13.2.el7.x86_64.rpm
+yum install -y https://fastdl.mongodb.org/tools/db/mongodb-database-tools-rhel80-x86_64-100.3.1.rpm
 
