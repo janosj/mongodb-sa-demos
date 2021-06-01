55da623060dc1a0ff3472c0eeda9fffa0c3d707e
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2021-06-01 17:01:56 -0400

    added Avro dependency for MongoDB Connector.

--- kafka-sensors-time-series/1-install-kafka/install-kafka.rhel.sh
+++ kafka-sensors-time-series/1-install-kafka/install-kafka.rhel.sh
@@ -76,11 +76,11 @@ fi
 # (when starting Kafka Connect) with an avro class not found error.
 # Using Sonatype, dependencies can be determined by looking at the 
 # kafka connect pom file within the Nexus repository Manager.
-# To find this jars at runtime, add the directory to your CLASSPATH
+# The location of these dependencies have to be in your CLASSPATH
+# when you start Kafka Connect.
 AVRO=avro-1.9.2
 echo "Installing dependencies..."
 curl http://archive.apache.org/dist/avro/$AVRO/java/$AVRO.jar --output $PLUGINS_DIR/$AVRO.jar
-export CLASSPATH=~/kafka_2.13-2.8.0/plugins/*;$CLASSPATH
 
 echo
 echo "Configuring ..."
