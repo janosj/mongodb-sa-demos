3e20378d82a12748cec349c76dbf702557e10477
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2021-07-27 16:32:32 -0400

    SSL Support

--- opsmgr/opsmgr-vm/start_containers.noSSL.sh
+++ opsmgr/opsmgr-vm/start_containers.noSSL.sh
@@ -26,7 +26,7 @@ done
 # Loop creating a set of Docker containers
 for i in $(seq 1 $NUMBER_TO_CREATE)
 do
-    cmd=" docker run -p 2702${i}:2702${i} -p 2703${i}:2703${i} -p 3306${i}:3306${i} "${ADD_HOST_LIST_PARAMS}" --hostname=myserver${i} -d --name automation-agent-container${i} -t mongo/automation-agent /opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent --mmsGroupId=${PROJECTID} --mmsApiKey=${APIKEY} --mmsBaseUrl=https://opsmgr:8443 -httpsCAFile=/etc/mongodb-mms/opsmgrCA.pem  -logLevel=INFO -logFile=/var/log/mongodb-mms-automation/automation-agent.log"
+    cmd=" docker run -p 2702${i}:2702${i} -p 2703${i}:2703${i} -p 3306${i}:3306${i} "${ADD_HOST_LIST_PARAMS}" --hostname=myserver${i} -d --name automation-agent-container${i} -t mongo/automation-agent /opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent --mmsGroupId=${PROJECTID} --mmsApiKey=${APIKEY} --mmsBaseUrl=http://opsmgr:8080 -logLevel=INFO -logFile=/var/log/mongodb-mms-automation/automation-agent.log"
     eval "${cmd}"
 done
 
